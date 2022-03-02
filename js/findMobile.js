7dc508f1bc096e1f49cad1db5f882ce6cc049add
MD RIGAN <moshiur251@gmail.com>
2022-03-02 16:58:21 +0600

    second-commit

--- js/findMobile.js
+++ js/findMobile.js
@@ -8,20 +8,23 @@ const searchMobile =() =>{
     searchInput.value = ''
 }
 const findMobiles= arr=>{
-    const parentElement = document.getElementById('search-result')
-    for(const phone of arr){
-        console.log(phone);
+    const parentElement = document.getElementById('search-result');
+    console.log(arr);
+    //remove previous content when search again
+    parentElement.textContent=''
+    arr.slice(0,20).forEach(phone => {
         const div = document.createElement('div')
+        div.classList.add('col')
         div.innerHTML=`
-        <div class="card" style="width: 18rem;">
-            <img src="${phone.image}" class="card-img-top" alt="...">
+        <div class="card rounded" style="width: 18rem;height: 22rem;">
+            <img src="${phone.image}" class="card-img-top w-50  p-3" alt="...">
             <div class="card-body">
               <h5 class="card-title">${phone.phone_name}</h5>
               <p class="card-text">${phone.slug}.</p>
-              <a href="#" class="btn btn-primary">Go somewhere</a>
+              <a href="#" class="btn btn-primary">Details</a>
             </div>
         </div>
         `;
         parentElement.appendChild(div);
-    }
+    });
 }
